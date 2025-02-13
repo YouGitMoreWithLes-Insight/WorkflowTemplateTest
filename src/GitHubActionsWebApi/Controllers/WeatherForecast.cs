ae4bb773c1ed24abbd270d38c39d8940411dd754
Les McWhirter <les.mcwhirter@insight.com>
2025-02-13 13:30:50 -0700

    Testing runtime environment variable #25

--- src/GitHubActionsWebApi/Controllers/WeatherForecast.cs
+++ src/GitHubActionsWebApi/Controllers/WeatherForecast.cs
@@ -34,7 +34,7 @@ namespace GitHubActionsWebApi.Controllers
     {
         public DateTime Date { get; set; }
         public int TemperatureC { get; set; }
-        public string Summary { get; set; }
+        public string Summary { get; set; } = string.Empty;
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
     }
