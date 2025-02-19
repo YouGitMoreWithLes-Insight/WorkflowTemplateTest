40ccf1f3e10074a637a6e9d985351a6cbfaff933
Les McWhirter <les.mcwhirter@insight.com>
2025-02-19 13:48:11 -0700

    Path test #18

--- src/InfrastructureAsCode/static-web.tf
+++ src/InfrastructureAsCode/static-web.tf
@@ -1,8 +1,12 @@
-resource "azurerm_static_web_app" "website" {
-  name                = format("%s-%s", local.base-name, "swa")
-  resource_group_name = azurerm_resource_group.deployment-rg[0].name
-  location            = azurerm_resource_group.deployment-rg[0].location
+resource "azurerm_storage_account" "static_website" {
+  name                     = format("%s%s", local.short-name, "swasa")
+  resource_group_name      = azurerm_resource_group.deployment-rg[0].name
+  location                 = azurerm_resource_group.deployment-rg[0].location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 
-  sku_tier = "Free"
-  
+  static_website {
+    index_document = "index.html"
+    error_404_document = "404.html"
+  }
 }
